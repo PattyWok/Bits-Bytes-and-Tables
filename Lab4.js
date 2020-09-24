4e7da9fb8223797a16494b62758f51f0fd2f785d
PattyWok <71677234+PattyWok@users.noreply.github.com>
2020-09-23 18:03:45 -0700

    Add files via upload
    Here are my updated files

---
+++ Lab4.js
@@ -0,0 +1,86 @@
+ /*The pussy way
+function pussyDiv(){
+	num = document.getElementById("bits").value
+	bytes = Math.pow(2, num);
+	document.getElementById('bytesText').innerHTML=bytes
+	console.log('HelloWorld');
+}
+pussyDiv();
+*/
+// console.log(document.getElementById('bits').value);
+// // document.getElementById('bits').onclick=
+//  function bytesGenerator(){
+// 	num = parseFloat(document.getElementById("bits").attribute.value);
+// 	console.log(num);
+// 	bytes = Math.pow(2, num);
+// 	document.getElementById("bits").value = bytes;
+// }
+// bytesGenerator();
+// var x = document.getElementById
+
+// function bytesGenerator(){
+// 	num = document.getElementById("bits").value;
+// 	num.getAttributeNode("value").value= 3;
+// }
+
+
+
+
+
+
+//USE THIS USE THIS
+// function bytesGenerator(){
+// 	newValue = document.getElementById("bits").value;
+// 	powa = Math.pow(2, newValue);
+// 	p = document.getElementById("bits");
+// 	t = p.value=powa + ` bytes!!`;
+// }
+
+
+
+
+
+
+
+
+
+
+//Need to set the value attribute of the element 'bits' to the value attribute of num
+
+
+
+// function textMover(){
+//	if (mouseover)
+// console.log(document.querySelectorAll("td")[1]);
+
+// function newq(){
+// 	var x = 0
+// for (x=0, i=0; i<document.querySelectorAll("table td:nth-of-type(i)"); x++, i++){
+// var result = document.querySelectorAll("table td:nth-of-type(x)");
+// }
+// console.log(result);
+// }
+// newq();
+// console.log(nodes);
+// for(var i=0; i<nodes.length; i++) {
+//     if (nodes[i].nodeName.toLowerCase() == '') {
+//          nodes[i].style.background = color;
+//      }
+// }
+// console.log(document.querySelectorAll(".parent").children);
+
+
+
+
+
+
+
+
+//USE THIS USE THIS// let children = document.getElementById(".poop1");
+// 	console.log(children);
+// 	q = children.childNodes;
+// 	console.log(q);
+// 	children.forEach(child => {
+// 	  child.style.color = "red";
+// 	})
+// 	console.log(child);
